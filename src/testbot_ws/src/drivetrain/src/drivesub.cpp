aec4fc057868ed77a7f24e9375ffc38b2985eb4e
cameron <ckiw23@gmail.com>
2024-08-28 16:30:49 +0000

    moved uart.h and uart.cpp into src and added code to node to transmit controller values over uart

--- src/testbot_ws/src/drivetrain/src/drivesub.cpp
+++ src/testbot_ws/src/drivetrain/src/drivesub.cpp
@@ -3,29 +3,43 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
+#include <string__struct.hpp>
+#include "uart.h"
 
 using std::placeholders::_1;
 
 class DriveSub : public rclcpp::Node
-{
+{  
+Uart testBotCommunications{};
 public:
   DriveSub() : Node("drivesub")
   {
-    subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
-      "/joy", 10, std::bind(&DriveSub::joy_callback, this, _1));
+    rclcpp::SubscriptionBase::SharedPtr
+        subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
+            "/joy", 10, std::bind(&DriveSub::joy_callback, this, _1));
   }
 
 private:
-  void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg) const
+  void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
   {
     RCLCPP_INFO(this->get_logger(), "Axes: [%f, %f]", msg->axes[0], msg->axes[1]);
     RCLCPP_INFO(this->get_logger(), "Buttons: [%d, %d]", msg->buttons[0], msg->buttons[1]);
+    float leftSpeed = 90 * msg-> axes[0];
+    float rightSpeed = 90 * msg-> axes[1];
+    std::string MessageToArduino = (std::to_string(leftSpeed) + "," + std::to_string(rightSpeed)).c_str();
+ 
+    testBotCommunications.sendUart(reinterpret_cast<unsigned char*>(const_cast<char*>(MessageToArduino.c_str())));
+    
   }
 
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+
+  ~DriveSub(){
+    testBotCommunications.closeUart();
+  }
 };
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<DriveSub>());
