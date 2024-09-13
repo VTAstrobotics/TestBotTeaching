3196683f0bdbac173ef3464d4970991dc94f9bec
cameron <ckiw23@gmail.com>
2024-09-13 17:35:53 +0000

    added comments for fill in the blank

--- drivetrain/src/drivesub.cpp
+++ drivetrain/src/drivesub.cpp
@@ -4,7 +4,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joy.hpp>
 #include <std_msgs/msg/string.hpp>
-#include "uart.h"
 
 using std::placeholders::_1;
 class DriveSub : public rclcpp::Node
@@ -12,26 +11,21 @@ class DriveSub : public rclcpp::Node
 Uart testBotCommunications{};
 public:
   DriveSub() : Node("drivetrainNode")
-  {
+  { // we've made the subscription but you need to make the two Float32 publishers you will need
     subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
       "/joy", 10, std::bind(&DriveSub::joy_callback, this, _1));
   }
 
-  ~DriveSub(){
-      testBotCommunications.closeUart();
-  }
 
 private:
   void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
   {
-    float leftSpeed  = msg-> axes[1];
-    float rightSpeed = msg-> axes[4];
+    /**
+     * here you need to take in the msg and seperate it into a left and right speed variable and publish those values to the /left and /right topic
+     */
 
-    std::string MessageToArduino = (std::to_string(leftSpeed) + "," + std::to_string(rightSpeed)).c_str();
-    RCLCPP_INFO(this->get_logger(), "Msg: [%s]", MessageToArduino.c_str());
 
-    testBotCommunications.sendUart_fb(reinterpret_cast<unsigned char*>(const_cast<char*>(MessageToArduino.c_str())));
-    testBotCommunications.readUart();
+
   }
 
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
