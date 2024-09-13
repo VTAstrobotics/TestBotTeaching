a75c6e0a0d0146c03a80172769de6c8b2a4d1612
cameron <ckiw23@gmail.com>
2024-09-13 17:30:43 +0000

    setting up project template

---
+++ build/drivetrain/CMakeFiles/drivetrainNode.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drivetrainNode.dir/src/drivesub.cpp.o"
+  "CMakeFiles/drivetrainNode.dir/src/drivesub.cpp.o.d"
+  "CMakeFiles/drivetrainNode.dir/src/uart.cpp.o"
+  "CMakeFiles/drivetrainNode.dir/src/uart.cpp.o.d"
+  "drivetrainNode"
+  "drivetrainNode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drivetrainNode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
