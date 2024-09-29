c9d0d27f36e035c5114d16fbe15ab9e5bb463c46
Tu Linh H Nguyen <138689312+linhnguyenhatu@users.noreply.github.com>
2024-09-29 14:50:36 -0400

    Add files via upload

--- drivetrain/src/drivesub.cpp
+++ drivetrain/src/drivesub.cpp
@@ -13,6 +13,9 @@ public:
   { // we've made the subscription but you need to make the two Float32 publishers you will need
     subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
       "/joy", 10, std::bind(&DriveSub::joy_callback, this, _1));
+    publisher1 = this->create_publisher<sensor_msgs::msg::float32>("Joy",10);
+    publisher2 = this->create_publisher<sensor_msgs::msg::float32>("Joy",10);
+
       //declare your two publishers here
   }
 
@@ -23,12 +26,22 @@ private:
     /**
      * here you need to take in the msg and seperate it into a left and right speed variable and publish those values to the /left and /right topic
      */
+    auto leftmsg = std_msgs::msg::float32();
+    leftmsg.data = msg->axes[1];
+    auto rightmsg = std_msgs::msg::float32();
+    leftmsg.data = msg->axes[4];
+    publisher1->pulish(leftmsg);
+    publisher2->pulish(rightmsg);
+
 
 
 
   }
 
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
+  rclcpp::Pulisher<sensor_msgs::msg::float32>::SharedPtr pulisher1;
+  rclcpp::Pulisher<sensor_msgs::msg::float32>::SharedPtr publisher2;
+
 
 };
 
