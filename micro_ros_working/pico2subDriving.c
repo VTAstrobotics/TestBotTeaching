186cc426b6c6b77bb97b30a41464f2998bd000b3
cameron <ckiw23@gmail.com>
2024-09-30 16:49:02 -0400

    Deleted extra comments

--- micro_ros_working/pico2subDriving.c
+++ micro_ros_working/pico2subDriving.c
@@ -143,9 +143,6 @@ int main() {
 
   rclc_node_init_default(&node, "pico_node", "", &support);
 
-  // rclc_subscription_init_default(&joy_Subscriber, &node,
-  // ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "/speeds");//TODO: can
-  // I just get the type or do I need the sequence type
  
   rclc_publisher_init_default(
       &publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
