5a49cec4547992c2bfb3708044ac440b8abe3471
Tu Linh H Nguyen <linhnguyenhatu@gmail.com>
2024-10-11 18:45:39 +0000

    Testbot Finalize

--- drivetrain/src/drivesub.cpp
+++ drivetrain/src/drivesub.cpp
@@ -3,7 +3,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joy.hpp>
-#include <std_msgs/msg/string.hpp>
+#include <std_msgs/msg/float32.hpp>
 
 using std::placeholders::_1;
 class DriveSub : public rclcpp::Node
@@ -13,10 +13,9 @@ public:
   { // we've made the subscription but you need to make the two Float32 publishers you will need
     subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
       "/joy", 10, std::bind(&DriveSub::joy_callback, this, _1));
-    publisher1 = this->create_publisher<sensor_msgs::msg::float32>("Joy",10);
-    publisher2 = this->create_publisher<sensor_msgs::msg::float32>("Joy",10);
-
       //declare your two publishers here
+    publisher1 = this->create_publisher<std_msgs::msg::Float32>("right",10);
+    publisher2 = this->create_publisher<std_msgs::msg::Float32>("left",10);
   }
 
 
@@ -26,22 +25,19 @@ private:
     /**
      * here you need to take in the msg and seperate it into a left and right speed variable and publish those values to the /left and /right topic
      */
-    auto leftmsg = std_msgs::msg::float32();
+    auto leftmsg = std_msgs::msg::Float32();
     leftmsg.data = msg->axes[1];
-    auto rightmsg = std_msgs::msg::float32();
-    leftmsg.data = msg->axes[4];
-    publisher1->pulish(leftmsg);
-    publisher2->pulish(rightmsg);
-
-
+    auto rightmsg = std_msgs::msg::Float32();
+    rightmsg.data = msg->axes[4];
+    publisher1->publish(rightmsg);
+    publisher2->publish(leftmsg);
 
 
   }
 
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
-  rclcpp::Pulisher<sensor_msgs::msg::float32>::SharedPtr pulisher1;
-  rclcpp::Pulisher<sensor_msgs::msg::float32>::SharedPtr publisher2;
-
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher1;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher2;
 
 };
 
